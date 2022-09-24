fd639b4a081eca2232e2bade1dd728906228ec4a
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2022-09-24 10:35:59 +0200

    * queryString korrekt in den Editor übergeben * unnötigen code entfernt

--- src/app/feature/journal/journal-new/journal-new.component.ts
+++ src/app/feature/journal/journal-new/journal-new.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {ApplicationService} from '../../application/service/application.service';
 
@@ -11,7 +11,7 @@ export interface Query {
   templateUrl: './journal-new.component.html',
   styleUrls: ['./journal-new.component.scss']
 })
-export class JournalNewComponent implements OnInit {
+export class JournalNewComponent implements OnInit,DoCheck {
 
   query?: Query;
   queryString?: String;
@@ -19,21 +19,33 @@ export class JournalNewComponent implements OnInit {
   currentDate: Date = new Date();
   applications: any;
   selectedApplication: any;
-  text: any;
+  // der text als Attribut in eine Object reinmachen, wo auch die application und weitere paramter festgehalten sind
+  text?: String;
+
 
   constructor(private router: Router, private applicationService: ApplicationService) { }
 
   ngOnInit(): void {
     this.query=history.state;
     this.queryString = this.query?.query;
+    console.log("queryString: " + this.queryString);
     if (this.queryString != null) {
-     this.showLabel = true;
+      this.showLabel = true;
+      this.text = '<h1>' + this.queryString + '</h1>';
+    } else {
+      this.text ='<h1>Seitentitel</h1>';
     }
-
-    this.applications = this.applicationService.getApplications()
+    this.applications = this.applicationService.getApplications();
   }
+
+
 
   main() {
-    this.router.navigateByUrl("main")
+    this.router.navigateByUrl("main");
   }
+
+  ngDoCheck(): void {
+    // wird bei jeder Aenderung im Input Feld des Editors verwendet
+  }
+
 }
